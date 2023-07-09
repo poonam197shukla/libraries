341118cf3e479c1e3d8df93142531657a98814d1
poonam shukla <poonam.shukla@siemens.com>
2023-07-09 13:08:15 +0530

    added new services/shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ControlDirectiveDirective } from './libraries/shared/directives/control-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlDirectiveDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/libraries/shared/directives/control-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ControlDirectiveDirective } from './control-directive.directive';
+
+describe('ControlDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ControlDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
